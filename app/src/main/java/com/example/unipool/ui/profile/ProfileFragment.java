4b27596c606ce82a1dc1976b5517610a64195a82
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2022-12-29 20:52:23 +0800

    add bottom nav bar

--- app/src/main/java/com/example/unipool/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/unipool/ui/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.unipool.ui.notifications;
+package com.example.unipool.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.unipool.databinding.FragmentNotificationsBinding;
+import com.example.unipool.databinding.FragmentHomeBinding;
 
-public class NotificationsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        ProfileViewModel homeViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
